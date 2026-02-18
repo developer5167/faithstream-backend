b8e0126112d2bd4455b1d2ecdc27409bdb98eab1
kruparaoKapil <krupagarao.g@kapilit.com>
2026-02-18 15:35:20 +0530

    changed some changes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const app = require('./app');
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, "0.0.0.0", () => {
   console.log(`FaithStream backend running on ${process.env.PORT}`);
 });
